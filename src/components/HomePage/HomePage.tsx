bfa2d261523ac381af6474d1bd7bb3bdf6c547bb
MasahiKon <konnonorth@gmail.com>
2023-09-03 01:53:46 -0700

    add anim2

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -8,11 +8,16 @@ const [effect, setEffect] = useState(false);
 
   return (
     <main>
-      <section className="flex justify-center items-center h-screen w-screen bg-cyan-400">
-        <div className={`cursor-pointer hover:text-xl active:text-sm select-none transition-{font-size} transition-{line-height} duration-200`} onClick={() => setEffect(true)}>
+      <section className="flex justify-center items-center h-screen w-screen">
+        <div className={`cursor-pointer hover:text-xl active:text-sm select-none transition-{font-size} transition-{line-height} duration-200`} onClick={() => setEffect(!effect)}>
           <h1>Hi, I&apos;m Masashi.</h1>
         </div>
-        <Image src={'/k0755_1.svg'} width={200} height={200} alt="tete" className={`absolute -left-96 top-96 origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && 'translate-x-80'}`}/>
+        <Image src={'/cloud1.svg'} width={200} height={200} alt="tete" className={`absolute select-none left-[10%] -top-[200px] origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && 'translate-y-[250px]'}`}/>
+        <Image src={'/tree1.svg'} width={200} height={200} alt="tete" className={`absolute select-none -left-[200px] bottom-[5%] origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && 'translate-x-[100px]'}`}/>
+        <Image src={'/tree1.svg'} width={200} height={200} alt="tete" className={`absolute select-none -left-[200px] bottom-[10%] origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && 'translate-x-[150px]'}`}/>
+        <Image src={'/tree1.svg'} width={200} height={200} alt="tete" className={`absolute select-none left-full bottom-[5%] origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && '-translate-x-[100px]'}`}/>
+        <Image src={'/tree1.svg'} width={200} height={200} alt="tete" className={`absolute select-none left-full bottom-[10%] origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && '-translate-x-[150px]'}`}/>
+        <Image src={'/deer1.svg'} width={100} height={100} alt="tete" className={`absolute select-none -left-[100px] bottom-[10%] origin-center hover:scale-[120%] active:scale-50 transition-{scale} duration-200 transition-{x} duration-200 ${effect && 'translate-x-[200px]'}`}/>
       </section>
     </main>
   );
