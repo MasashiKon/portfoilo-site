0fbca04d75f5623ebffd958c3b138d1693a03e95
MasahiKon <konnonorth@gmail.com>
2023-09-05 02:14:12 -0700

    tweak button

--- src/components/Header.tsx/Header.tsx
+++ src/components/Header.tsx/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
   return (
     <div className="absolute h-20 w-screen z-10 flex justify-between items-center p-5 pt-10">
       <div>
-        <p>Header</p>{" "}
+        <p>Header</p>
         <button
           onClick={() => dispatch(removeItem(LocalStrageValue.is_started))}
         >
@@ -19,7 +19,7 @@ function Header() {
         </button>
       </div>
       <div>
-        <button className="rounded-full bg-olivine w-20 h-20 hover:bg-yellow-green active:shadow-[inset_5px_5px_8px_8px_rgba(0,0,0,0.3)] transition-colors duration-300">
+        <button className="rounded-full bg-yellow-green w-20 h-20 md:bg-olivine md:hover:bg-yellow-green active:shadow-[inset_5px_5px_8px_8px_rgba(0,0,0,0.3)] transition-colors duration-300">
           <HiOutlineSearchCircle className="w-20 h-20 active:scale-95"/>
         </button>
       </div>
