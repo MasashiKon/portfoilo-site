092969f12a1858f3ecffc49fedcd20fc994c62e4
MasahiKon <konnonorth@gmail.com>
2023-09-05 02:07:29 -0700

    add check button

--- src/components/Header.tsx/Header.tsx
+++ src/components/Header.tsx/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useDispatch } from "react-redux";
+import { HiOutlineSearchCircle } from "react-icons/hi";
 
 import { removeItem } from "@/redux/reducers/localStrageSlice";
 import { LocalStrageValue } from "@/localStrage/localStrageValues";
@@ -8,11 +9,20 @@ import { LocalStrageValue } from "@/localStrage/localStrageValues";
 function Header() {
   const dispatch = useDispatch();
   return (
-    <div className="absolute z-10">
-      <p>Header</p>
-      <button onClick={() => dispatch(removeItem(LocalStrageValue.is_started))}>
-        reset
-      </button>
+    <div className="absolute h-20 w-screen z-10 flex justify-between items-center p-5 pt-10">
+      <div>
+        <p>Header</p>{" "}
+        <button
+          onClick={() => dispatch(removeItem(LocalStrageValue.is_started))}
+        >
+          reset
+        </button>
+      </div>
+      <div>
+        <button className="rounded-full bg-olivine w-20 h-20 hover:bg-yellow-green active:shadow-[inset_5px_5px_8px_8px_rgba(0,0,0,0.3)] transition-colors duration-300">
+          <HiOutlineSearchCircle className="w-20 h-20 active:scale-95"/>
+        </button>
+      </div>
     </div>
   );
 }
