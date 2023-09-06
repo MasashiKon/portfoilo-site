307ecfdba3b5ee596c4be00bb94a3abca5cf5679
MasahiKon <konnonorth@gmail.com>
2023-09-06 02:57:39 -0700

    80

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -43,7 +43,7 @@ const HomePage = () => {
   }, [isStarted, dispatch]);
 
   useEffect(() => {
-    if (scrollY >= bodyHeight - innerWidth - 83) {
+    if (scrollY >= bodyHeight - innerWidth - 80) {
       dispatch(setIsTutorialMet(true));
     } else if (isTutorialMet) {
       dispatch(setIsTutorialMet(false));
