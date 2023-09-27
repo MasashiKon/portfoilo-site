61f752c0ddf3df8cfa130a7e425248926cc8bdf1
MasahiKon <konnonorth@gmail.com>
2023-09-26 23:09:21 -0700

    add simple 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const Custom404 = () => {
+  return (
+    <main>
+      <div className="pt-28 flex justify-center items-center h-screen w-screen">
+        <p>Sorry, this page doesn&apos;t exist.</p>
+      </div>
+    </main>
+  );
+};
+
+export default Custom404;
